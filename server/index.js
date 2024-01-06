4f4ded6ea165746a621141b8d594438bb993ad90
Aravinth PM <110657801+aravinthpm77@users.noreply.github.com>
2024-01-06 12:04:39 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ app.use(bodyParser.json())
 app.use('/uploads', express.static('uploads'))
 
 
-const PORT =process.env.PORT || 5000
+const PORT =process.env.PORT || "https://react-socialogram.onrender.com"
 
 
 const  CONNECTION_URL = 'mongodb+srv://admin:admin@socialcommunity.xvgsu5l.mongodb.net/?retryWrites=true&w=majority';
